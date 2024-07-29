ad21fc525d7f6e5695b4add212e32c80945ad640
Kuba <tornadowro@gmail.com>
2024-07-29 20:58:55 +0200

    10 current  2024-07-29 20:58:52  24.05.3346.8c5066250910      6.6.42                          *

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    zed-editor
     unzip
     vscode
     git
