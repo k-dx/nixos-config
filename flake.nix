13ec0e76d4a80aa30d0b060dffa0d4657a3569e7
Kuba <tornadowro@gmail.com>
2024-08-30 18:40:46 +0200

    18 current  2024-08-21 18:39:35  24.05.20240805.883180e       6.6.44                          *

--- flake.nix
+++ flake.nix
@@ -4,16 +4,23 @@
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
     # Please replace my-nixos with your hostname
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = {inherit inputs;};
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
+        inputs.home-manager.nixosModules.default
       ];
     };
   };
