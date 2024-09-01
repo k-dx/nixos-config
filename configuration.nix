9b00945c80579519d72ae0c01c4f6f8fbe3db97e
Kuba <tornadowro@gmail.com>
2024-09-01 12:39:19 +0200

    26 current  2024-09-01 12:39:14  24.05.20240805.883180e       6.6.44                          *

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-unstable, ... }:
 
 {
   imports =
@@ -106,19 +106,19 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    keepassxc
-    pcloud
-    gnomeExtensions.appindicator
-    gnomeExtensions.pop-shell
-    libnotify
-    zed-editor
-    unzip
-    vscode
-    git
-    gnome.gnome-tweaks
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
+  environment.systemPackages = [ # with pkgs; [
+    pkgs-unstable.pcloud
+    pkgs.keepassxc
+    pkgs.gnomeExtensions.appindicator
+    pkgs.gnomeExtensions.pop-shell
+    pkgs.libnotify
+    pkgs.zed-editor
+    pkgs.unzip
+    pkgs.vscode
+    pkgs.git
+    pkgs.gnome.gnome-tweaks
+    pkgs.vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    pkgs.wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
--- flake.nix
+++ flake.nix
@@ -5,17 +5,24 @@
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs: {
     # Please replace my-nixos with your hostname
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos = let
+        pkgs-unstable = import nixpkgs-unstable {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+      in nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      # specialArgs = {inherit inputs;};
+      specialArgs = {inherit pkgs-unstable;};
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
