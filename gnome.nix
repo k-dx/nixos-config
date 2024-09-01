d8952bb7ecea9856b66d858cb0e40c34e25288a2
Kuba <tornadowro@gmail.com>
2024-09-01 20:14:18 +0200

    45 current  2024-09-01 20:03:09  24.05.20240805.883180e       6.6.44                          *

---
+++ gnome.nix
@@ -0,0 +1,80 @@
+{ config, pkgs, ... }:
+
+{
+# based on https://heywoodlh.io/nixos-gnome-settings-and-keyboard-shortcuts
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      document-font-name="Source Sans 3 Ultra-Light 13";
+      font-name="Source Sans 3 Ultra-Light 13";
+      monospace-font-name="Source Code Pro 13";
+      text-scaling-factor = "1.25";
+      show-battery-percentage=true;
+    };
+    "org/gnome/shell" = {
+       enabled-extensions = [
+         "pop-shell@system76.com"
+         "appindicatorsupport@rgcjonas.gmail.com"
+       ];
+       favorite-apps = [
+         "firefox.desktop"
+         "org.gnome.Console.desktop"
+         "code.desktop"
+         "org.gnome.Nautilus.desktop"
+         "dev.zed.Zed.desktop"
+      ];
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      minimize = ["<Super>w"];
+      activate-window-menu = "disabled";
+      toggle-quick-settings = "disabled";
+      restore-shortcuts = "disabled";
+      #toggle-message-tray = "disabled";
+      close = ["<Super>q"];
+      #maximize = "disabled";
+      move-to-monitor-down = "disabled";
+      move-to-monitor-left = "disabled";
+      move-to-monitor-right = "disabled";
+      move-to-monitor-up = "disabled";
+      move-to-workspace-left = ["<Shift><Super>h" "<Shift><Super>k"];
+      move-to-workspace-right = ["<Shift><Super>l" "<Shift><Super>j"];
+      move-to-workspace-down = "disabled";
+      move-to-workspace-up = "disabled";
+      switch-to-workspace-left=["<Control><Super>k" "<Control><Super>h" "<Control><Super>Left"];
+      switch-to-workspace-right=["<Control><Super>j" "<Control><Super>l" "<Control><Super>Right"];
+      #toggle-maximized = ["<Super>m"]';
+      #unmaximize = "disabled";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+        #next = [ "<Shift><Control>n" ];
+        #previous = [ "<Shift><Control>p" ];
+        #play = [ "<Shift><Control>space" ];
+        custom-keybindings = [
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
+        ];
+      };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "launch terminal";
+      command = "kgx";
+      binding = "<Super>t";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+      name = "launch files";
+      command = "nautilus";
+      binding = "<Super>f";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+      name = "launch browser";
+      command = "firefox";
+      binding = "<Super>b";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
+      name = "screenshot";
+      command = "flameshot";
+      binding = "<Super><Shift>s";
+    };
+  };
+}
