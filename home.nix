83ffb41cb3b7d0403060ca40008be7b3ff95df98
Kuba <tornadowro@gmail.com>
2024-09-01 14:08:00 +0200

    33 current  2024-09-01 14:07:55  24.05.20240805.883180e       6.6.44                          *

--- home.nix
+++ home.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./firefox.nix
+  ];
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "kuba";
