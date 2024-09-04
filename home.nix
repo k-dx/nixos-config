b98c18c656154c386ac39ca056bb53d440a18d8d
Kuba <tornadowro@gmail.com>
2024-09-04 18:55:17 +0200

    50 current  2024-09-04 18:55:07  24.05.20240805.883180e       6.6.44                          *

--- home.nix
+++ home.nix
@@ -80,5 +80,9 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-
+  programs.git = {
+    enable = true;
+    userName  = "k-dx";
+    userEmail = "tornadowro@gmail.com";
+  };
 }
