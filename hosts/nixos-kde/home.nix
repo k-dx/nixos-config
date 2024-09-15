4c017b67534f7f1fc4211d1c013c8bdd12bd66af
k-dx <tornadowro@gmail.com>
2024-09-15 15:49:17 +0200

    7 current   2024-09-13 22:56:51  24.05.20240805.883180e   6.6.44                          *

--- hosts/nixos-kde/home.nix
+++ hosts/nixos-kde/home.nix
@@ -76,7 +76,7 @@
   #  /etc/profiles/per-user/kuba/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "emacs";
   };
 
   # Let Home Manager install and manage itself.
