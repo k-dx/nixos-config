56a67b0b7f7d1275d45322b77c60264ecfa76de9
k-dx <tornadowro@gmail.com>
2024-09-25 19:43:15 +0200

    11 current  2024-09-25 19:43:12  24.05.20240805.883180e   6.6.44                          *

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -26,7 +26,6 @@
     };
     "org/gnome/shell/keybindings" = {
       toggle-quick-settings = "disabled";
-      move-to-workspace-left = ["<Shift><Super>o" "<Shift><Super>k"];
     };
     "org/gnome/desktop/wm/keybindings" = {
       minimize = ["<Super>w"];
@@ -40,7 +39,7 @@
       move-to-monitor-left = "disabled";
       move-to-monitor-right = "disabled";
       move-to-monitor-up = "disabled";
-      # move-to-workspace-left = ["<Shift><Super>h" "<Shift><Super>k"];
+      move-to-workspace-left = ["<Shift><Super>h"];
       move-to-workspace-right = ["<Shift><Super>L" "<Shift><Super>j"];
       move-to-workspace-down = "disabled";
       move-to-workspace-up = "disabled";
