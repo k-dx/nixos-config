f4b07d73f07d7dac24748cee65e675f6bac87efa
k-dx <tornadowro@gmail.com>
2024-10-19 10:12:03 +0200

    27 current  2024-10-19 10:11:48  24.05.20240805.883180e  6.6.44                          *

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -22,6 +22,17 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
+  # systemd-resolved configuration needed for Mullvad VPN
+  # https://nixos.wiki/wiki/Systemd-resolved
+  networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    dnsovertls = "true";
+  };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
